ed2c7c26f545b876841d2082389e20325114e8a2
Letícia Carvalho <lc-leticiacarvalho@hotmail.com>
2025-05-05 13:15:57 +0000

    Desafio ok!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -20,3 +20,65 @@ int main() {
 
     return 0;
 }
+// Desafio Super Trunfo -Países
+// Tema 1 - Cadastro de Cartas
+
+int main(){
+   
+    int codigocidade;
+    char nomedacidade[50];
+    float area;
+    int população;
+    float pib;
+    int pontosturisticos;
+
+      // Cadastrar as cartas:
+
+    printf ("Cadastro da cidade" \n);
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%d", &codigocidade);
+    
+    printf("Digite o nome da cidade:  ");
+    scanf("%s", &nomedacidade);
+
+    printf("Digite a area da cidade:  ");
+    scanf("%f", &area);
+
+    printf("Digite a população:  ");
+    scanf("%d", &população);
+
+    printf("Digite o valor do pib: (Em bilhões):  ");
+    scanf("%lf", &pib);
+
+    printf("Digite os pontos turisticos:  ");
+    sacanf("%d", &pontosturisticos)
+
+      // Exibir as cartas:
+
+    printf("Carta cadastrada");
+    
+    printf("Codigo: %d\n", codigocidade);
+    printf("Nome: %s\n", nomedacidade);
+    printf("Area: %f\n", area);
+    printf("População: %d\n", população);
+    printf("Pib: %lf", pib);
+    printf("Pontos turisticos: %d\n", pontosturisticos)
+
+    return 0;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
